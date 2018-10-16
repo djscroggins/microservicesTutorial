c01f77ba6f602855293ebbf988feebd70c5260ae
David <>
2018-10-15 21:03:46 -0500

    Added testing, fixed CLI issue, one test still failing, db env var seems fine in image, will need to retrace db setup etc.

---
+++ testdriven-app/services/users/project/tests/test_config.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+
+from flask import current_app
+from flask_testing import TestCase
+from project import app
+
+
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.DevelopmentConfig')
+        return app
+
+    def test_app_is_development(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertFalse(current_app is None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            os.environ.get('DATABASE_URL')
+        )
+
+
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.ProductionConfig')
+        return app
+
+    def test_app_is_production(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertFalse(app.config['TESTING'])
+
+
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.ProductionConfig')
+        return app
+
+    def test_app_is_production(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertFalse(app.config['TESTING'])
+
+
+if __name__ == '__main__':
+    unittest.main()
